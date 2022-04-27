eade3753ea042e2e3852f2ca2a10d4796016af0d
Alexander Demidovskij <monadv@yandex.ru>
2022-04-27 08:00:00 +0300

    initial structure

---
+++ benchmark/methods/topsis/demo.py
@@ -0,0 +1,14 @@
+"""
+TOPSIS implementation based on https://link.springer.com/book/10.1007/978-3-642-48318-9#about
+
+Ching-Lai Hwang, Kwangsun Yoon. Multiple Attribute Decision Making Methods and Applications.
+A State-of-the-Art Survey.
+"""
+
+
+def main():
+    print('Running TOPSIS method')
+
+
+if __name__ == '__main__':
+    main()
