5dbadb5f24ac2a30fa90050f58df642d73bccadd
Alexander Demidovskij <monadv@yandex.ru>
2022-05-10 11:25:35 +0300

    creating bin folder for ml-ldm

--- .config/venv_setup.sh
+++ .config/venv_setup.sh
@@ -14,4 +14,6 @@ python -m pip install -r requirements.txt
 python -m pip install -r requirements_qa.txt
 
 curl -L -o lingvo-dss-all.jar https://github.com/ldss-hse/ldss-core-aggregator/releases/download/decision_maker_v0.7/lingvo-dss-all.jar
+ls -la benchmark/methods/ml_ldm/scripts
+mkdir -p benchmark/methods/ml_ldm/scripts/bin
 mv lingvo-dss-all.jar benchmark/methods/ml_ldm/scripts/bin/
